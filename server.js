a0721868b1f2ca6c6fd8899e2eb398689ead2b0e
gabriel-perez1 <g.perez.carbo@gmail.com>
2022-01-18 20:34:58 -0500

    html routes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require ('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -112,6 +114,10 @@ app.post('/api/animals', (req, res) => {
   const animal = createNewAnimal(req.body, animals);
   res.json(animal);
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
